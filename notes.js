694d8f297d9c07ce96879fcc8559f2b2ead21204
Tanha <nusrattanha897@gmail.com>
2024-05-08 21:57:20 +0600

    what is api and examples

--- notes.js
+++ notes.js
@@ -1,3 +1,12 @@
 /**
+ * API stands for Application Programming Interface
+ * 01. how to communicate?
+ * How different technologies communicate - API. API actually helps to send data and protocol from one technology/application/server/system to another. 
  * 
+ * 02. Laziness
+ * getting data from third parties. example- weather application , login with google.
+ * Example of API: you - food panda - restaurant - you. You order something from food panda/zomato. Food panda takes the responsibility to cook your ordered food and deliver it to your door. Here food panda works as an API which communicates you with restaurant. 
  */
+
+// github user api - api.github.com/users/user-name
+// randomuser.me. to read apis use json formatter.
