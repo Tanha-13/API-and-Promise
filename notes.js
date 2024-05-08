32276a7d5c660dabaaace7862705bbb40fa8d1f1
Tanha <nusrattanha897@gmail.com>
2024-05-08 22:59:45 +0600

    XMLHttpRequest

--- notes.js
+++ notes.js
@@ -10,3 +10,15 @@
 
 // github user api - api.github.com/users/user-name
 // randomuser.me. to read apis use json formatter.
+
+// topic: XMLHttpRequest
+/**
+ * XMLHttpRequest.readyState
+ * -> 0 - UNSENT  - Client has been created. open() not called yet.
+ * -> 1 - OPENED  - open() has been called.
+ * -> 2 - HEADERS_RECEIVED  - send() has been called, and headers and status are available.
+ * -> 3 - LOADING Downloading; responseText holds partial data.
+ * -> 4 - DONE  - The operation is complete.
+ * 
+ * XMLHttpRequest is a method to receive and send data.
+ */
